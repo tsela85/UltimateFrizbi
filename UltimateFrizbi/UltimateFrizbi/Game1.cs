3d0e11bc523c52f6037f83cdc84931e9da47c7f4
Tom Sela <tsela85@gmail.com>
2012-03-29 01:19:54 +0200

    2 are moving, still need to fix key breaks

--- UltimateFrizbi/UltimateFrizbi/Game1.cs
+++ UltimateFrizbi/UltimateFrizbi/Game1.cs
@@ -20,6 +20,7 @@ namespace UltimateFrizbi
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
 
+        SpriteFont font;
         GraphicsDevice device;
         Texture2D goalTexture; // Texture for the goals
         Texture2D fieldTexture; // Texture for the goals
@@ -71,18 +72,20 @@ namespace UltimateFrizbi
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
             device = graphics.GraphicsDevice;
-            goalTexture = Content.Load<Texture2D>(@"goal");
-            fieldTexture = Content.Load<Texture2D>(@"Football_field");
             screenWidth = device.PresentationParameters.BackBufferWidth;
             screenHeight = device.PresentationParameters.BackBufferHeight;
-            player1Goal = new Rectangle(20, (screenHeight / 2) - 64, 32, 128);
-            player2Goal = new Rectangle(screenWidth - 52, (screenHeight / 2) - 64, 32, 128);
+            //font
+            font = Content.Load<SpriteFont>(@"font");
+            // field
+            goalTexture = Content.Load<Texture2D>(@"goal");
+            fieldTexture = Content.Load<Texture2D>(@"Football_field");
+            player1Goal = new Rectangle(42, (screenHeight / 2) - 80, 32, 160);
+            player2Goal = new Rectangle(screenWidth - 70, (screenHeight / 2) - 80, 32, 160);
             fieldRectnagle = new Rectangle(0, 0, screenWidth, screenHeight);
             // Load the player resources 
-            redPlayer.Initialize(Content.Load<Texture2D>(@"player"), new Vector2(screenWidth-211,screenHeight/2),Color.OrangeRed);
-            bluePlayer.Initialize(Content.Load<Texture2D>(@"player"), new Vector2(105, screenHeight / 2), Color.BlueViolet);
+            redPlayer.Initialize(Content.Load<Texture2D>(@"playerR"), new Vector2(screenWidth-211,screenHeight/2),Color.OrangeRed,font,screenHeight,screenWidth);
+            bluePlayer.Initialize(Content.Load<Texture2D>(@"playerL"), new Vector2(105, screenHeight / 2), Color.BlueViolet, font, screenHeight, screenWidth);
 
-            // TODO: use this.Content to load your game content here
         }
 
         /// <summary>
@@ -106,7 +109,7 @@ namespace UltimateFrizbi
                 this.Exit();
 
             // TODO: Add your update logic here
-
+            redPlayer.Update();
             base.Update(gameTime);
         }
 
@@ -133,6 +136,6 @@ namespace UltimateFrizbi
             spriteBatch.Draw(fieldTexture, fieldRectnagle, Color.White);
             spriteBatch.Draw(goalTexture, player1Goal, Color.Blue);
             spriteBatch.Draw(goalTexture, player2Goal, Color.Red);
-        }
+            }
     }
 }
--- UltimateFrizbi/UltimateFrizbi/player.cs
+++ UltimateFrizbi/UltimateFrizbi/player.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
 
 
 namespace UltimateFrizbi
@@ -13,7 +14,8 @@ namespace UltimateFrizbi
         public Vector2 Position;
         public bool hasFrizbi;
         public Rectangle rec;
-        public bool Active;
+        public int power;
+        public float angle;
         // TODO: add atributes in the future
     }
 
@@ -23,41 +25,118 @@ namespace UltimateFrizbi
         const int size = 30;
 
         public Texture2D PlayerTexture;
+        SpriteFont playerFont;
         Color playerColor;
         int playerScore;
         pivot[] pivots;
+        int currentPivot;
+        public bool pivotChosen;
+        int screenHeight, screenWidth;
 
         public int Score
         {
             get { return playerScore; }
         }
 
-        public void Initialize(Texture2D texture, Vector2 position,Color color)
+        public void Initialize(Texture2D texture, Vector2 position,Color color, SpriteFont font,int sHeight,int sWidth)
         {
 
             PlayerTexture = texture;
+            playerFont = font;
             playerColor = color;
             playerScore = 0;
+            currentPivot = 0;
+            pivotChosen = false;
+            screenHeight = sHeight;
+            screenWidth = sWidth;
+
             pivots = new pivot[numberOfPivots];
 
             for (int i = 0; i < numberOfPivots; i++)
             {
                 pivots[i].Position = position + new Vector2(i*40,-size/2);
                 pivots[i].hasFrizbi = false;
-                pivots[i].Active = false;
                 pivots[i].rec = new Rectangle((int)pivots[i].Position.X,(int)pivots[i].Position.Y, size, size);
+                pivots[i].power = 0;
+                pivots[i].angle = 0;
 
             }
         }
 
+        private void ProcessKeyboard()
+        {
+            KeyboardState keybState = Keyboard.GetState();
+            if (!pivotChosen)
+            {
+                if (keybState.IsKeyDown(Keys.Left))
+                {
+                    currentPivot = (currentPivot + 1) % 3;             
+                }
+                if (keybState.IsKeyDown(Keys.Right))
+                    currentPivot = (currentPivot + 1) % 3;
+                //choose player
+                if (keybState.IsKeyDown(Keys.Enter))
+                {
+                    pivotChosen = true;
+                }
+            } else
+            {
+                //Angle
+                if (keybState.IsKeyDown(Keys.Left))
+                    pivots[currentPivot].angle = (pivots[currentPivot].angle - 0.01f) % (2 * MathHelper.Pi);
+                if (keybState.IsKeyDown(Keys.Right))
+                    pivots[currentPivot].angle = (pivots[currentPivot].angle + 0.01f) % (2 * MathHelper.Pi); 
+                //power
+                if (keybState.IsKeyDown(Keys.Down))
+                    pivots[currentPivot].power -= 1;
+                if (keybState.IsKeyDown(Keys.Up))
+                    pivots[currentPivot].power += 1;
+                //fix power
+                if (pivots[currentPivot].power > 100)
+                        pivots[currentPivot].power = 100;
+                if (pivots[currentPivot].power < 0)
+                    pivots[currentPivot].power = 0;
+                //move pivot
+                if (keybState.IsKeyDown(Keys.Enter))
+                {
+                    calcPivotPosition(currentPivot);
+                    pivotChosen = false;
+                }
+            }
+        }
+
+        private void calcPivotPosition(int pivot)
+        {
+            pivots[currentPivot].Position.X += (float)Math.Cos(pivots[currentPivot].angle) * pivots[currentPivot].power/10;
+            if (pivots[currentPivot].Position.X > screenWidth - 69)
+                pivots[currentPivot].Position.X = screenWidth - 69;
+            if (pivots[currentPivot].Position.X < 42)
+                pivots[currentPivot].Position.X = 42;
+            pivots[currentPivot].Position.Y += (float)Math.Sin(pivots[currentPivot].angle) * pivots[currentPivot].power/10;
+            if (pivots[currentPivot].Position.Y > screenHeight - 50)
+                pivots[currentPivot].Position.Y = screenHeight - 50;
+            if (pivots[currentPivot].Position.Y < 20)
+                pivots[currentPivot].Position.Y = 20;            
+            pivots[currentPivot].rec.X = (int)pivots[currentPivot].Position.X;
+            pivots[currentPivot].rec.Y = (int)pivots[currentPivot].Position.Y;
+        }
+
         public void Update()
         {
+            ProcessKeyboard();
         }
 
         public void Draw(SpriteBatch spriteBatch)
         {
             for (int i = 0; i < numberOfPivots; i++) {
-                spriteBatch.Draw(PlayerTexture,pivots[i].rec,playerColor);
+                if (i != currentPivot)
+                    spriteBatch.Draw(PlayerTexture, pivots[i].rec, null,playerColor);
+                else
+                {
+                    spriteBatch.Draw(PlayerTexture, pivots[i].rec, null, Color.Tan);
+                    spriteBatch.DrawString(playerFont, "Power:" + pivots[i].power + "\nAngle:" +
+                        (int)MathHelper.ToDegrees(pivots[i].angle), pivots[i].Position + (pivots[i].Position.Y < screenHeight - 100 ? new Vector2(0, 50) : -new Vector2(0, 50)), Color.Black);
+                }
             }
         }
     }
